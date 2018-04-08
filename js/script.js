6346afdf47da415e9e361fe987bda0d709cbfa3b
Alexander Logosh <mr.logosh@gmail.com>
2018-04-08 20:12:25 +0300

    добавил панораму, изменил слайдер, внес пару исправлений

--- js/script.js
+++ js/script.js
@@ -5,9 +5,29 @@ $(function(){
   autoplaySpeed: 2000,
   infinite: true,
   speed: 1000,
-  slidesToShow: 1,
-  adaptiveHeight: true
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  centerMode: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    }
+    ]
 });
-
-		
 })
+
+var title = document.querySelector(".map__title");
+var panorama = document.querySelector(".map__panorama");
+panorama.classList.add("visually-hidden");
+
+title.addEventListener("click", function(event) {
+	title.classList.toggle("map__title--bottom-corners");
+	panorama.classList.toggle("visually-hidden");
+});
