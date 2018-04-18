6333ea73b42a3dc40f8efd4986f0f27c2619ce9a
Alexander Logosh <mr.logosh@gmail.com>
2018-04-19 00:12:24 +0300

    подправил новости и страницу с номерами

--- js/script-gallery.js
+++ js/script-gallery.js
@@ -1,18 +1,23 @@
-var btnYellow = document.querySelector(".rooms__quantity--left a");
-var btnGreen = document.querySelector(".rooms__quantity--right a");
+var btnLeft = document.querySelector(".rooms__quantity--left a");
+var btnRight = document.querySelector(".rooms__quantity--right a");
 var galleryYellow = document.querySelector(".rooms__gallery-block--yellow");
 var galleryGreen = document.querySelector(".rooms__gallery-block--green");
 galleryGreen.classList.add("visually-hidden");
+btnLeft.classList.add("rooms__quantity--activ");
 
-btnYellow.addEventListener("click", function(event) {
+btnLeft.addEventListener("click", function(event) {
 	event.preventDefault();
 	galleryYellow.classList.toggle("visually-hidden");
 	galleryGreen.classList.toggle("visually-hidden");
+	btnLeft.classList.toggle("rooms__quantity--activ");
+	btnRight.classList.toggle("rooms__quantity--activ");
 });
 
-btnGreen.addEventListener("click", function(event) {
+btnRight.addEventListener("click", function(event) {
 	event.preventDefault();
 	galleryYellow.classList.toggle("visually-hidden");
 	galleryGreen.classList.toggle("visually-hidden");
+	btnLeft.classList.toggle("rooms__quantity--activ");
+	btnRight.classList.toggle("rooms__quantity--activ");
 });
 
